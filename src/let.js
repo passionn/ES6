3de9d5186a2270c7c7310a8e7d8d5bd49d002fcd
yzhangpan <yzhangpan@gmail.com>
2016-08-12 00:10:53 +0800

    update let const

--- src/let.js
+++ src/let.js
@@ -1,5 +1,26 @@
+//let 定义 块级作用于；
 {
     let name='passion';
     console.log(name);
 }
-console.log(name);
+//console.log(name);
+
+//＊ 注意：不存在变量的提升；必须先声明后使用；
+//＊ 暂时行死区；但是babel 在转换过程中处理了这中错误；
+var tmp = 123;
+if (true) {
+  tmp = 'abc'; // ReferenceError
+  console.log(tmp);
+  let tmp;
+}
+
+//* 不允许重复声明；
+{
+    let name=123;
+    //let name=456; //报错
+}
+
+//const 声明的变量是只读的不能改变；但是可以为其增加属性；
+
+//全局变量；var function 声明的全局变量依然是全局变量的属性；
+//let class const 声明的对象不属于全局；
