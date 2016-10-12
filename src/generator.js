a094bffa15cd306aa3e47b102333fe272fe828a3
zhangpan <yzhangpan@gmail.com>
2016-10-12 11:59:25 +0800

    update generoter

--- src/generator.js
+++ src/generator.js
@@ -7,3 +7,15 @@ var generator=f();
 setTimeout(function(){
     generator.next();
 },2000);
+
+function *hello(){
+    yield 'hello';
+    yield 'world';
+    return 'ending';
+}
+
+var hw=hello();
+console.log(hw.next());
+console.log(hw.next());
+console.log(hw.next());
+console.log(hw.next());
