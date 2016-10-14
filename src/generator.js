6a452d1732425b5ec4368cc81130a2850d33d3bf
zhangpan <yzhangpan@gmail.com>
2016-10-14 19:20:29 +0800

    update

--- src/generator.js
+++ src/generator.js
@@ -19,3 +19,13 @@ console.log(hw.next());
 console.log(hw.next());
 console.log(hw.next());
 console.log(hw.next());
+
+function *vervield(){
+    var a=yield 2;
+    var b=yield 3;
+    return 5;
+}
+
+var v1=vervield();
+console.log('变量！');
+console.log(v1.next());
