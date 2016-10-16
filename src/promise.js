f25d8a81e8210f45a06a193244fe04c08eeb373c
yzhangpan <yzhangpan@gmail.com>
2016-10-16 22:08:28 +0800

    update promise

--- src/promise.js
+++ src/promise.js
@@ -32,3 +32,55 @@ loadImageAsync('aaa').then((value)=>{
 },(error)=>{
     console.log(error);
 });
+
+//promise，链式调用
+
+function promiseChar(val){
+    return new Promise((resolve,reject)=>{
+        setTimeout(function(){
+            resolve(val+2);
+        },3000);
+    });
+}
+
+function promiseChar1(val){
+    return new Promise((resolve,reject)=>{
+        setTimeout(function(){
+            resolve(val+2);
+        },2000);
+    });
+}
+
+//哎呀 这样不行啊！！！
+promiseChar(5).then(function(value){
+    console.log(value);
+    return value;
+}).then(promiseChar1)
+.then(function(value){
+    console.log(value);
+});
+
+var p1=Promise.resolve(1);
+p1.then(promiseChar)
+.then(promiseChar1);
+
+function doubleUp(value) {
+    return value * 2;
+}
+function increment(value) {
+    return value + 1;
+}
+function output(value) {
+    console.log(value);// => (1 + 1) * 2
+}
+
+/*var promise = Promise.resolve(1);
+promise
+    .then(increment)
+    .then(doubleUp)
+    .then(output)
+    .catch(function(error){
+        // promise chain中出现异常的时候会被调用
+        console.error(error);
+    });
+    */
