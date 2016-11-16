c4ef5979e2a3cebae25c2c54a386e7eadc4ce14e
zhangpan <yzhangpan@gmail.com>
2016-11-16 18:13:57 +0800

    beatiful

--- src/export.js
+++ src/export.js
@@ -1,28 +1,31 @@
 /*
-* 导出默认的对象;
-*/
-export default function(){
-	console.log('hello export');
+ * 导出默认的对象;
+ */
+export default function() {
+    console.log('hello export');
 }
 
 /*
-* 分别导出多个变量 通过import {} 导入。和下面的 导出是一样的；
-*/
-export const NAME='zhangpan';
-export const NAMEA='passionn';
+ * 分别导出多个变量 通过import {} 导入。和下面的 导出是一样的；
+ */
+export const NAME = 'zhangpan';
+export const NAMEA = 'passionn';
 
-const name='passion';
-const age=25;
+const name = 'passion';
+const age = 25;
 
 /*
-* 导出对象 必须通过 import {name,age} 引用，对象可以省略不需要的属性； 
-*/
-export {name,age};
+ * 导出对象 必须通过 import {name,age} 引用，对象可以省略不需要的属性；
+ */
+export {
+    name,
+    age
+};
 
 /*注意*/
 /*
-* export 命令是规定的对外接口，必须与模块内壁的变量建立一一对应关系；
-*/
+ * export 命令是规定的对外接口，必须与模块内壁的变量建立一一对应关系；
+ */
 
 // 下面的代码会报错，因为导出的是了一个变量而不是一个接口；
 
@@ -32,8 +35,7 @@ export {name,age};
 */
 //正确写法；
 
-var m=1;
-export {m}; //或者 export var m=1;
-
-
-
+var m = 1;
+export {
+    m
+}; //或者 export var m=1;
