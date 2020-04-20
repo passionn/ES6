d53c55295c10392c0876a6c27551916156efabee
yzhangpan <yzhangpan@gmail.com>
2020-04-20 11:20:05 +0800

    回顾

--- src/string.js
+++ src/string.js
@@ -1,1 +1,13 @@
-string.js
+let s='hello \n word';
+console.log(s);
+
+// 多行字符串
+let string1 =`欢迎来到
+    我的世界！`;
+console.log(string1);
+
+// 字符串模版；
+let name="passion";
+let age=21;
+let s2=`我的姓名是${name},我的年龄是 ${age}`;
+console.log(s2);
